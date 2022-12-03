079a5cb661dffe0d4408ea9d034c5a7112b513dc
ujjawal sharma <112757910+Sharmaujjawal123@users.noreply.github.com>
2022-12-03 18:55:02 +0530

    many code of javascript like loop code ,conditional etc

---
+++ op.js
@@ -0,0 +1,65 @@
+document.write("average of marks")
+var name = prompt("are you know javascriptt")
+window.alert("average of marks of student")
+
+document.write("</br>")
+document.write("</br>")
+var maths = 20;
+document.write("maths:--",maths)
+document.write("</br>")
+document.write("</br>")
+
+var eng=40;
+document.write("eng:--",eng)
+document.write("</br>")
+document.write("</br>")
+var phy=50;
+document.write("phy:--",phy)
+document.write("</br>")
+document.write("</br>")
+var Bio= 60;
+document.write("Bio:--",Bio)
+document.write("</br>")
+document.write("</br>")
+var chem= 70;
+document.write("chem:--",chem)
+document.write("</br>")
+document.write("</br>")
+var python=80;
+document.write("python:--",python)
+document.write("</br>")
+document.write("</br>")
+document.write("</br>")
+document.write("</br>")
+var average=(maths+eng+phy+Bio+chem+python)/6
+var average=average.toFixed(2)
+document.write("average:--", average)
+document.write("</br>")
+if (average<10){
+    
+     document.write("yes")
+    }    
+else if(average>10)
+    document.write("no")
+
+
+
+
+    document.write("</br>")
+    document.write("</br>")
+    document.write("</br>")
+    document.write("</br>")    
+
+var grade="d"
+document.write("</br>")
+switch (grade)
+{
+
+case ("a"):document.write("grade b")
+break;
+case ("b"):document.write("grade b")
+break;
+case ("c"):document.write("grade c")  
+break;
+default:document.write("no grade") 
+} 
